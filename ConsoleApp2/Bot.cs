83114584bfe5b3501dc0eaf0c4e53f5c20cef744
alexcoralt <alexcoralt@1ASIX-PC15->
2017-11-28 12:17:11 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp2/Bot.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+using TLSharp.Core;
+
+namespace ConsoleApp2
+{
+    class Bot
+    {
+        private TelegramClient client;
+
+        public Bot(int api_id, string api_hash)
+        {
+            this.client = new TelegramClient(api_id, api_hash);
+        }
+
+        public async Task Connect()
+        {
+            await this.client.ConnectAsync();
+        }
+
+        public async Task Authenticate(String phoneNumber)
+        {
+            var hash = await client.SendCodeRequestAsync(phoneNumber);
+
+            var code = "94306"; // you can change code in debugger
+
+            var user = await client.MakeAuthAsync(phoneNumber, hash, code);
+        }
+
+        public async Task SendMessage(string phoneNum)
+        {
+            //get available contacts
+            var result = await client.GetContactsAsync();
+
+            //find recipient in contacts
+            var user = result.Users
+                .Where(x => x.GetType() == typeof(TLUser))
+                .Cast<TLUser>()
+                .FirstOrDefault(x => x.Phone == phoneNum);
+
+            //send message
+            await client.SendMessageAsync(new TLInputPeerUser() { UserId = user.Id }, "OUR_MESSAGE");
+        }
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot(68128, "b6212e15b78cca650d1ced280a7640fc");
+
+            Authenticate(bot).Wait();
+
+            bot.SendMessage("663453087").Wait();
+        }
+
+        static async Task Authenticate(Bot bot)
+        {
+            await bot.Connect();
+            await bot.Authenticate("34663453087");
+        }
+
+
+    }
+}
