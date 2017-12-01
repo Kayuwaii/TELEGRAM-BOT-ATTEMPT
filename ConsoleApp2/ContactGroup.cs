591d7bd46d8230778bc232524255c5cb2e69295c
alexcoralt <alexcoralt@1ASIX-PC15->
2017-12-01 14:00:05 +0100

    Groups

---
+++ ConsoleApp2/ContactGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+
+namespace Test
+{
+    class ContactGroup
+    {
+        public string Name;
+        public List<TLUser> Members;
+        public string Description;
+
+        public ContactGroup(string name, List<TLUser> members, string description)
+        {
+            Name = name;
+            Members = members;
+            Description = description;
+        }
+    }
+}
